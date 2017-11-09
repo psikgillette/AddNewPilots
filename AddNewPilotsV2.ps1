ef9b79591d082dc4495bbbcf4d8933bc9464bafa
Kris Gillette <kgillette@planesense.com>
2017-11-09 14:31:29 -0500

    Add files
    Add script, template xlsx, and csv.
    Added info to ReadMe.

---
+++ AddNewPilotsV2.ps1
@@ -0,0 +1,50 @@
+﻿Import-Module ActiveDirectory
+
+$ErrorActionPreference = "Stop"
+
+$logfile = "C:\PowerShell\AddNewPilots.log"
+
+$Password = "Password1"
+
+$Users = Import-Csv -Path "C:\PowerShell\NewPilots.csv"
+
+ForEach($User in $Users){
+    try{
+        $FullName = $User.Last + ", " + $User.First
+        $SAM = $User.First.Substring(0,1) + $User.Last
+        $UPN = $SAM + "@star.dcu"
+        $TemplateUser = Get-ADUser -Identity $User.Template -Properties Manager, Department, Description, Title, Company
+        $TemplateGroups = (Get-ADUser -Identity $User.Template -Properties MemberOf).MemberOf
+
+        Add-Content $logfile "Adding $FullName $(Get-Date)" 
+
+        New-ADUser -Name $FullName -Instance $Template `
+            -Path "OU=Pilots,OU=Star Users,DC=star,DC=dcu" `
+            -GivenName $User.First -Surname $User.Last -Initials $User.Initials `
+            -StreetAddress $User.Address -City $User.City -State $User.State -PostalCode $User.Zip `
+            -Country US -HomePhone $User.HomePhone -MobilePhone $User.CellPhone `
+            -SamAccountName $SAM -UserPrincipalName $UPN -DisplayName $FullName `
+            -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
+            -ChangePasswordAtLogon $true -Enabled $true -ErrorAction Stop
+
+        Start-Sleep -Milliseconds 550
+
+        ForEach($Group in $TemplateGroups){
+            Try {
+                Add-ADGroupMember $Group -Members $SAM
+            }
+            Catch {
+                Add-Content $logfile "Error for $FullName - $($_.Exception.Message)" -PassThru
+            }
+        }
+    }
+    catch [System.UnauthorizedAccessException]{
+        Write-Output "You must be a domain administrator to run this script."
+    }
+    catch [System.ServiceModel.FaultException]{
+        Write-Output "User $FullName already exists." | Add-Content $logfile -PassThru
+    }
+    catch {
+        Add-Content $logfile "Error for $FullName - $($_.Exception.Message)" -PassThru
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AddNewPilots
-Adding new pilot users to AD based on CSV input.
+This script was created to automate the creation process for new pilots. A new hire roster is sent to the helpdesk,
+usually sent by Eric Cannon. The information from that email is then copied and transposed into the Excel 
+template "NewPilotTemplate".This template takes the information, breaks up address fields and selects an appropriate 
+template account based on the pilot's reporting base. Once the information is entered, it should be Saved As 
+a csv file with the name "NewPilots" in the same directory. The script can then be run, referencing the newly 
+created file.The script sets variables such as the temporary password for the user account and the location of the 
+csv file. It then goes into a loop for each user listed in the file and sets the variables for Full name, samAccount 
+name, and user profile name. Once the variables are set, it creats a new user based on the appropriate template, and
+sets the properties of the user based on the information in the CSV.
